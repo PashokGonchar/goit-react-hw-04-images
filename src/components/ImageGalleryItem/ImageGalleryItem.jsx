d88e34e3c1436300a374973f09c19b8dce64d718
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-08-11 13:50:31 +0300

    modal_close

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -12,7 +12,7 @@ export const ImageGalleryItem = ({ isLoading, images }) => {
   };
 
   const handleCloseModal = () => {
-    setIsModalOpen(true);
+    setIsModalOpen(false);
   };
 
   return (
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,18 +2,17 @@ import React, { useEffect } from 'react';
 import { OverlayDiv, ModalDiv, ImageModal } from './Modal.styled';
 
 const Modal = ({ imageUrl, onClose }) => {
-  const handleKeyDown = event => {
-    if (event.key === 'Escape') {
-      onClose();
-    }
-  };
-
   useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
     document.addEventListener('keydown', handleKeyDown);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  });
+  }, [onClose]);
 
   return (
     <OverlayDiv onClick={onClose}>
